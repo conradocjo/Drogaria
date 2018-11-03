4c4096b5ef9affb1832f62e9cdd54dc9e55b7098
Conrado Jardim de Oliveira <conradocjo@gmail.com>
2018-11-03 18:53:06 -0300

    Modificações realizadas

--- Drogaria/src/main/java/br/com/drogaria/bean/BeanModelo.java
+++ Drogaria/src/main/java/br/com/drogaria/bean/BeanModelo.java
@@ -1,7 +1,18 @@
 package br.com.drogaria.bean;
 
+import java.util.List;
+import java.util.Map;
+
+import br.com.drogaria.util.RelatorioJasper;
+
 public class BeanModelo {
 
-	
-	
+	public void gerarRelatorioPDF(Map<String, Object> parametros, String caminho, List<?> dados, String nomeArquivo) {
+		RelatorioJasper relatorio = new RelatorioJasper();
+		try {
+			relatorio.gerarRelatorio(parametros, caminho, dados, nomeArquivo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
